45765d3b28beae4c5293b170aa3f6bcc129eae35
DVLP <pawel@example.com>
2024-02-22 03:00:53 +0000

    Added script to link mods into /mods folder; added .log and .sqlite3 into .gitignore

---
+++ tools/mount.bat
@@ -0,0 +1,5 @@
+SET modsPath=D:\SteamLibrary\steamapps\common\Cyberpunk 2077\bin\x64\plugins\cyber_engine_tweaks\mods
+
+mklink /d "%modsPath%\DisappearanceOf8ug8ear" "%~dp0\..\DisappearanceOf8ug8ear\"
+mklink /d "%modsPath%\QuestRunner" "%~dp0\..\QuestRunner\"
+mklink /d "%modsPath%\QuestTemplate" "%~dp0\..\QuestTemplate\"
