0d7578d945dca22ee25a6f0eaf0f35e1ba08de35
DVLP <pawel@example.com>
2024-03-26 20:20:55 +0000

    Added Spanish translation

---
+++ QuestRunner/locale/es-es.lua
@@ -0,0 +1,12 @@
+-- Translation thanks to Losco
+local Translation = {
+	["es-es"] = {
+		["1dentity"] = "1dentity",
+		contactSubtitle = "Servicios profesionales para necesidades modernas",
+		welcomeMessage = "Este es mi número de teléfono",
+		cancel = "Cancelar",
+		time_min = "min",
+		time_s = "s",
+	}
+}
+return Translation
