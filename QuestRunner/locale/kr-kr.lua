320a7e5971d7fc68d70cff7dddd8ac516c03aa6b
DVLP <pawel@example.com>
2024-07-29 19:58:10 +0100

    Added Korean translation

---
+++ QuestRunner/locale/kr-kr.lua
@@ -0,0 +1,11 @@
+local Translation = {
+	["kr-kr"] = {
+		["1dentity"] = "1dentity",
+		contactSubtitle = "현대적 필요를 위한 전문 서비스",
+		welcomeMessage = "제 번호입니다",
+		cancel = "취소",
+		time_min = "분",
+		time_s = "초",
+	}
+}
+return Translation
